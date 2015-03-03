d06fc8d8e6cf09047ec91622361f22d7047c7af7
Jose Hugo De La Cruz Romero <hugo.delacruz@live.com.mx>
2015-03-03 11:14:17 -0600

    Hardcoded text was moved to new module

--- cli.py
+++ cli.py
@@ -2,166 +2,134 @@ import getpass
 import gateway
 import subprocess 
 import os
+import text
 
 
 ws_server_pid = None
 
 # Web Services Server 
 def stop_ws_server():
-	print 'Stopping Web Server...   [', ws_server_pid, ']'
-	if ws_server_pid != None:
-		os.system('taskkill /F 	/PID ' + str(ws_server_pid))
-		print 'Server stopped.'
-	else:
-		print 'Server was not running.'	
+    print text.STOPPING_WS % ws_server_pid  
+    if ws_server_pid != None:
+        os.system('taskkill /F     /PID ' + str(ws_server_pid))
+        print text.SRV_STP
+    else:
+        print text.SRV_NR
 
-def start_ws_server():		
-	print 'Starting Web Server...',
-	global ws_server_pid
-	ws_server_pid = subprocess.Popen(r'Python ws_server.py').pid
-	print '[',ws_server_pid , ']'
-
+def start_ws_server():        
+    print text.SRV_STR,
+    global ws_server_pid
+    ws_server_pid = subprocess.Popen(r'Python ws_server.py').pid
+    print text.SRV_ID % ws_server_pid 
 
 def restart_ws_server():
-	stop_ws_server()
-	start_ws_server()
+    stop_ws_server()
+    start_ws_server()
 
 # Commands 
 #--
 def start_ws():
-	show_services()
-	
-	wsid = raw_input('\n\nId: ')
-	
-	rfc = gateway.get_single_RFC(wsid)
-	
-	if len(rfc) == 0:
-		print '[Error] Wrong ID'			
-		return False 
+    show_services()
+    
+    wsid = raw_input(text.CLI_ID)
+    
+    rfc = gateway.get_single_RFC(wsid)
+    
+    if len(rfc) == 0:
+        print text.ERR_WID            
+        return False 
 
-	gateway.update_rfc_status(wsid, '1')
-	restart_ws_server()
-	
+    gateway.update_rfc_status(wsid, '1')
+    restart_ws_server()
+    
 
 # --
 def show_services():
-	status = { '0' : 'Not running',
-			   '1' : 'Running'
-	}
+    status = { '0' : 'Not running',
+               '1' : 'Running'
+    }
 
-	services = gateway.getRFCs() 
-	print """
-+==============================================================	
-| Services    \t\t|     Status  
-+============================================================== """
-	for service in services:
-		print ' ', service[0] +':', service[1], '\t\t', status[service[2]]
+    services = gateway.getRFCs() 
+    print text.SRV_HEAD
+    for service in services:
+        print text.SRV_LINE % (service[0], service[1], service[2])
 
 
-#--		
+#--        
 def conf_sap_server():
-	print """
-New SAP Server configuration
-============================
-	"""
+    print text.SRV_CFG_HD
+    name   = raw_input(text.SRV_NAME)
+    ip     = raw_input(text.SRV_HOST)
+    sysnr  = raw_input(text.SRV_SYSNR)
+    client = raw_input(text.SRV_MANDT)
+    user   = raw_input(text.SRV_USER)
+    passwd = getpass.getpass(text.SRV_PASS)
 
-	name   = raw_input('Server name: ')
-	ip     = raw_input('IP/Hostname: ')
-	sysnr  = raw_input('System number: ')
-	client = raw_input("Mandant: ")
-	user   = raw_input('User: ')
-	# passwd = raw_input('Password: ')
-	passwd = getpass.getpass('Password: ')
-
-	gateway.insert_config(name, ip, sysnr, client, user, passwd)
+    gateway.insert_config(name, ip, sysnr, client, user, passwd)
 
 
-
-#--	
+#--    
 def new_ws():
-	if not gateway.isConnected():
-		print 'You must configure a SAP connection first, type `config` '
-		return False
+    if not gateway.isConnected():
+        print text.MSG_CFSAP
+        return False
 
-	print """
-New Web Service
-===============	
-	"""
+    print text.NWS_HEAD
+    rfc = raw_input(text.RFC_NAME)
+    rfc = rfc.upper().strip()
 
-	rfc = raw_input('RFC Name: ')
-	rfc = rfc.upper().strip()
+    if gateway.existsRFCinDB(rfc):
+        print text.ERR_RFCEXS
+        return False 
+        
+    if not      gateway.existsRFCinSAP(rfc):
+        print text.ERR_RFCDNE # rfc does not exist!
+        return False 
 
-	if gateway.existsRFCinDB(rfc):
-		print 'RFC Existente en la base de datos'
-		return False 
-		
-	if not 	 gateway.existsRFCinSAP(rfc):
-		print 'RFC no existe en SAP'
-		return False 
+        
+    gateway.insert_rfc(rfc)    
 
-		
-	gateway.insert_rfc(rfc)	
-
-	print 'Se ha creado el servicio para la RFC'
+    print text.MSG_WSCR # Service have been created
 
 
 
 
 #--
 def exit():
-	stop_ws_server()
-	print 'Bye'
-	return True 
-	
-	 
-
+    stop_ws_server()
+    print text.MSG_BYE
+    return True 
+    
 
 #--
 def help_():
-	print """
-Available commands:
-===================
-
-	exit   - Exit 
-	config - Configure SAP Connection 
-	new    - Create a new Web Service	
-	"""
-	return False	
+    print text.CLI_HELP
+    return False    
 
 execute_command = {
-	'config'   : conf_sap_server,
-	'configure': conf_sap_server,
-	'show'     : show_services,
-	'start'    : start_ws,	
-	'new'      : new_ws,
-	'help'     : help_,
-	'exit'     : exit,
-	'quit'     : exit
-	}
+    'config'   : conf_sap_server,
+    'configure': conf_sap_server,
+    'show'     : show_services,
+    'start'    : start_ws,    
+    'new'      : new_ws,
+    'help'     : help_,
+    'exit'     : exit,
+    'quit'     : exit
+    }
 
 
 # Starts command line interface 
 if __name__ == '__main__':
-	print """
---------------------------------------------------------------	
-rfc2ws v0.05 [2012] - @hugo_dc
-==============================================================
+    print 
+    exit = False 
+    while(exit != True):
+        command = raw_input('\n>>> ')
+        command = command.replace(' ', '').lower()
 
-Crea servicios webs a partir de RFCs.
-Escribe `help` para ver la lista de comandos disponibles.
-
---------------------------------------------------------------
-	"""
-
-	exit = False 
-	while(exit != True):
-		command = raw_input('\n>>> ')
-		command = command.replace(' ', '').lower()
-
-		if command in execute_command.keys():
-			exit = execute_command[command]()
-		else:
-			if len(command.strip()) > 1:		
-				print "Command `"+command+"` not found"	
+        if command in execute_command.keys():
+            exit = execute_command[command]()
+        else:
+            if len(command.strip()) > 1:        
+                print text.ERR_CMNF % command
 
 
---
+++ text.py
@@ -0,0 +1,61 @@
+
+STOPPING_WS = "Stoping Web Server... [%s]"
+SRV_STP     = 'Server stopped...'
+SRV_NR      = 'Server not running.'    
+SRV_STR     = 'Starting Web Server...'
+SRV_ID      = '[%s]'
+CLI_ID      = '\n\nId: '
+SRV_HEAD    = """
++==============================================================    
+| Services    \t\t|     Status  
++============================================================== """
+SRV_LINE    = " %s : %s, \t\t, %s"
+SRV_CFG_HD  = """
+New SAP Server configuration
+============================
+    """
+SRV_NAME    = 'Server name: '
+SRV_HOST    = 'IP/Hostname: '
+SRV_SYSNR   = 'System number: '
+SRV_MANDT   = "Mandant: "
+SRV_USER    = 'User: '
+SRV_PASS    = 'Password: '
+
+MSG_CFSAP   = 'You must configure a SAP connection first, type `config` '
+
+NWS_HEAD    = """
+New Web Service
+===============    
+    """
+
+RFC_NAME    = 'RFC Name: '
+
+ERR_RFCEXS  = 'RFC already exists!'
+ERR_WID     = '[Error] Wrong ID'
+ERR_RFCDNE  = 'RFC does not exists in SAP system!'
+
+MSG_WSCR    = 'Service has been created for RFC'
+MSG_BYE     = 'Bye'
+
+CLI_HELP   = """
+Available commands:
+===================
+
+    exit   - Exit 
+    config - Configure SAP Connection 
+    new    - Create a new Web Service    
+    """
+
+CLI_HEAD = """
+--------------------------------------------------------------    
+rfc2ws v0.05 [2012] - @hugo_dc
+==============================================================
+
+Create Web Service from SAP RFCs.
+Type `help` to see a list of available commands
+
+--------------------------------------------------------------
+    """
+
+ERR_CMNF = "Command `%s` not found!"
+
